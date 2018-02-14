86227d4dc7414669db6cf73a73738c8860954716
Artur Tarasenko <arttarawork@gmail.com>
2018-02-14 18:02:53 -0500

    returns the origin gate properly

--- flightaware-wrapper.py
+++ flightaware-wrapper.py
@@ -8,10 +8,11 @@ from zeep import xsd
 from zeep.cache import SqliteCache
 from zeep.transports import Transport
 import collections
+import time
 
-username = 'username'
-password = 'password'
-flightId = 'flightId'
+username = input("FlightAware API Username:\t")
+password = input("FlightAware API Password:\t")
+flightId = input("Flight Number:\t")
 
 session = Session()
 session.auth = HTTPBasicAuth(username, password)
@@ -20,6 +21,19 @@ session.auth = HTTPBasicAuth(username, password)
 transport = Transport(cache=SqliteCache(), session=session)
 client = Client('http://flightxml.flightaware.com/soap/FlightXML2/wsdl', transport = transport)
 
-response = zeep.helpers.serialize_object(client.service.FlightInfo('DL2409', 1), target_cls=collections.OrderedDict)
+response0 = zeep.helpers.serialize_object(client.service.FlightInfo(flightId, 15), target_cls=collections.OrderedDict)
 
-print(response)
+flight = 0
+
+while((flight < 14) and ((response0['flights'][flight + 1]['filed_departuretime'] - int(time.time())) > 0)):
+    flight += 1
+
+filed_departuretime = response0['flights'][flight]['filed_departuretime']
+
+faFlightID = zeep.helpers.serialize_object(client.service.GetFlightID(flightId, filed_departuretime), target_cls=collections.OrderedDict)
+
+response1 = client.service.AirlineFlightInfo(faFlightID)
+
+originGate = response1['gate_orig']
+
+print(originGate)
